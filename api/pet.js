f21c62ed013374ce81c8ce69d68e921e132da937
Vladislav-Ivanchikov <sexxrab@gmail.com>
2025-03-17 20:36:31 +0300

    fix errors with DB and server 2"

--- api/pet.js
+++ api/pet.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import e from "express";
 dotenv.config();
 const app = express();
 app.use(json());
@@ -20,45 +21,47 @@ async function connectToDB() {
   }
 }
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
+// const port = 3000;
+// app.listen(port, () => {
+//   console.log(`Сервер запущен на порту ${port}`);
+// });
+
+connectToDB().catch((e) => console.error("Error connecting to DB:", e));
+
+app.get("/api/pet/:userID/:petName", async (req, res) => {
+  const { petName, userID } = req.params;
+  try {
+    if (!db) {
+      throw new Error("DB not connected");
+    }
+    const collection = db.collection("pets");
+    const pet = await collection.findOne({ userID, petName });
+    res.json(pet || { error: "Pet not found" });
+  } catch (e) {
+    console.error("Error getting pet:", e);
+    res.status(500).send("Error getting pet");
+  }
 });
 
-connectToDB()
-  .then(() => {
-    app.get("/api/pet/:userID/:petName", async (req, res) => {
-      const { petName, userID } = req.params;
-      try {
-        const collection = db.collection("pets");
-        const pet = await collection.findOne({ userID, petName });
-        res.json(pet || { error: "Pet not found" });
-      } catch (e) {
-        console.error("Error getting pet:", e);
-        res.status(500).send("Error getting pet");
-      }
-    });
-
-    app.post("/api/pet/:userID/:petName", async (req, res) => {
-      const { petName, userID } = req.params;
-      try {
-        const collection = db.collection("pets");
-        const petData = { userID, petName, ...req.body };
-        delete petData._id;
-        await collection.updateOne(
-          { userID, petName },
-          { $set: petData },
-          { upsert: true }
-        );
-        res.status(200).send("Pet saved");
-      } catch (e) {
-        console.error("Error saving pet:", e);
-        res.status(500).send("Error saving pet");
-      }
-    });
-  })
-  .catch((e) => {
-    console.error("Error connecting to DB:", e);
-  });
+app.post("/api/pet/:userID/:petName", async (req, res) => {
+  const { petName, userID } = req.params;
+  try {
+    if (!db) {
+      throw new Error("DB not connected");
+    }
+    const collection = db.collection("pets");
+    const petData = { userID, petName, ...req.body };
+    delete petData._id;
+    await collection.updateOne(
+      { userID, petName },
+      { $set: petData },
+      { upsert: true }
+    );
+    res.status(200).send("Pet saved");
+  } catch (e) {
+    console.error("Error saving pet:", e);
+    res.status(500).send("Error saving pet");
+  }
+});
 
 export default app;
